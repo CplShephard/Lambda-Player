540ffdebd9ec9d856c972502ff8ae1067b1d77a8
CplShephard <166573134+CplShephard@users.noreply.github.com>
2026-06-27 22:32:31 +0300

    Evdangelion1.0fixes

--- app/src/main/java/dev/shephard/player/ui/screens/PlaylistScreen.kt
+++ app/src/main/java/dev/shephard/player/ui/screens/PlaylistScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.itemsIndexed
